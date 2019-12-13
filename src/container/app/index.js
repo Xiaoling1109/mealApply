5ceac0578617d4f33f48b1cf82eb9fafcaab0cfb
王君瑶 <wangjunyao@wangjunyaodeMacBook-Pro.local>
2019-12-13 14:34:07 +0800

    up

--- src/container/app/index.js
+++ src/container/app/index.js
@@ -10,7 +10,8 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            datePickerShow: false
+            datePickerShow: false,
+            slected:'launch'
         }
     }
     onChange(date, dateString) {
@@ -20,27 +21,33 @@ class App extends Component {
         })
     }
     render() {
-        const { datePickerShow } = this.state
+        const { datePickerShow,slected } = this.state
 
         return (
             <React.Fragment>
                 <style dangerouslySetInnerHTML={{ __html: Style }} />
-                <div className="container">
-                    <div className="mealName">
-                        <div className="name">申请姓名：</div>
-                        <div>
-                            <input placeholder="请输入申请人的姓名" />
+                <div className="containee">
+                    <div className="container">
+                        <div className="mealName">
+                            <div className="name">申请姓名：</div>
+                            <div>
+                                <input placeholder="请输入申请人的姓名" />
+                            </div>
+
                         </div>
+                        <div className="mealDate">
+                            <div className="date">申请日期：</div>
+                            <div>
+                                <DatePicker onChange={() => this.onChange()} placeholder="选择日期" />
+                            </div>
+                        </div>
+                        <div className="mealType">
+                            <div className="type">用餐类型：</div>
+                            <span style={slected=="launch"?{background:'#FFDB55'}:null}>午餐</span><span style={slected=="dinner"?{background:'#FFDB55'}:null}>晚餐</span>
+                        </div>
+                        <div className="applyMeal">申请用餐</div>
 
                     </div>
-                    <div className="mealDate">
-                        <div className="date">申请日期：</div>
-                        <div>
-                            <DatePicker onChange={() => this.onChange()} placeholder="选择日期" />
-                        </div>
-                    </div>
-                    <div className="applyMeal">申请用餐</div>
-
                 </div>
 
             </React.Fragment>
